9f05eebab20681858030c15ffbd314522cf50134
sonu kumar <sonupandit969371@gmail.com>
2023-06-19 11:15:10 +0530

    eslint

--- src/app/api/comment/route.js
+++ src/app/api/comment/route.js
@@ -3,22 +3,25 @@ import { verifyJwtToken } from "@/lib/jwt";
 import Comment from "@/models/Comment";
 
 export async function POST(req){
-    await db.connect();
+    await db.connect()
 
-    const accessToken = req.headers.get("authorization");
-    const token = accessToken.split(" ")[1];
-    const decodedToken = verifyJwtToken(token);
-    if(!accessToken || !decodedToken){
-        return new Response(JSON.stringify({msg: "unauthorized (wrong or expire token)"}), {status: 404});
+    const accessToken = req.headers.get('authorization')
+    const token = accessToken.split(" ")[1]
+
+    const decodedToken = verifyJwtToken(token)
+
+    if (!accessToken || !decodedToken) {
+        return new Response(JSON.stringify({ error: "unauthorized (wrong or expired token)" }), { status: 403 })
     }
 
     try {
-        const body = await req.json();
-        let newComment = await Comment.create(body);
-        newComment = await newComment.populate("authorId");
-        return new Response(JSON.stringify(newComment), {status:200});
+       const body = await req.json()
+       
+       let newComment = await Comment.create(body)
+       newComment = await newComment.populate('authorId')
 
+       return new Response(JSON.stringify(newComment), {status: 201})
     } catch (error) {
-        return new Response(JSON.stringify(null), {status: 500});
+        return new Response(JSON.stringify(null), {status: 500})
     }
 }
